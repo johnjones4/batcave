d78ef2756bf5d95ea258dc5bf18e10c3254547f9
John Jones <johnjones4@gmail.com>
2021-08-02 14:53:37 -0400

    ready!

--- src/hal9000/util/event_log.go
+++ src/hal9000/util/event_log.go
@@ -17,6 +17,7 @@ type LogEventRow struct {
 }
 
 func InitLogger() error {
+	_ = os.Mkdir(os.Getenv("LOG_DIR"), os.ModePerm|os.ModeDir)
 	logFile := path.Join(os.Getenv("LOG_DIR"), fmt.Sprintf("%d.txt", int(time.Now().Unix())))
 	f, err := os.OpenFile(logFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
@@ -32,7 +33,7 @@ func LogEvent(event string, info interface{}) {
 		fmt.Println(err)
 		return
 	}
-	_, err = file.WriteString(string(eventBytes))
+	_, err = file.WriteString(string(eventBytes) + "\n")
 	if err != nil {
 		fmt.Println(err)
 		return
