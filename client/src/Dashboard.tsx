68c903b9add5afdf36588b1642a636764b9d0a96
John Jones <john@johnjonesfour.com>
2021-10-12 21:15:04 -0400

    disable news

--- client/src/Dashboard.tsx
+++ client/src/Dashboard.tsx
@@ -50,7 +50,7 @@ export default class Dashboard extends Component<DashboardProps,DashboardState> 
         { this.state.response && this.state.response.iframes.map(iframe => (<IFrameWidget iframe={iframe} key={iframe.url} />)) }
         { this.state.response && this.state.response.weather.map(weather => (<ImageWidget src={weather.radarURL} key={weather.radarURL} title='Radar' />)) }
         { this.state.response && this.state.response.weather.map(weather => (<WeatherWidget weather={weather} key={weather.radarURL} />)) }
-        { this.state.response && (<NewsWidget news={this.state.response.news} />) }
+        {/* { this.state.response && (<NewsWidget news={this.state.response.news} />) } */}
       </div>
     )
   }
